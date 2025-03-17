80de222a6bd1f4808fe2676f1ba029ac4706cbb1
amirrezasokhankhosh <amirrezasokhankhosh79@gmail.com>
2025-03-17 16:03:22 -0500

    Results completed.

--- perfed/node.py
+++ perfed/node.py
@@ -86,9 +86,9 @@ class Node:
             transform=ToTensor()
         )
 
-        portions = (self.num_nodes * (self.num_nodes + 1) * (2*self.num_nodes + 1)) // 6
+        portions = self.num_nodes * (self.num_nodes + 1) // 2
         portion_size = len(train_dataset) // portions
-        data_portion = portion_size * ((self.index + 1) ** 2)   
+        data_portion = portion_size * (self.index + 1)    
         indexes = [random.randint(0, len(train_dataset) - 1)
                    for _ in range(data_portion)]
         test_portion = len(test_dataset) // self.num_nodes
